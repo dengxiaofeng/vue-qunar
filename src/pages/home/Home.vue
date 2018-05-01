38f207d57509629d5cd0614dcb3da4cd40bd162f
dengxiaofeng <1186313486@qq.com>
2018-05-01 16:12:43 +0800

    src init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>Hello World</div>
+</template>
+
+<script type="text/ecmascript-6">
+    import HomeHeader from './compnents/Header'
+	export default {
+		name: 'Home',
+		compnents: {
+			HomeHeader
+		}
+	}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus"></style>
