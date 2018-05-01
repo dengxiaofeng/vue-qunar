7a8040b5e859ec9f5653b98ea00690b02ff4479b
dengxiaofeng <1186313486@qq.com>
2018-05-01 22:29:56 +0800

    change

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,16 +1,14 @@
 <template>
-  <swiper :options="swiperOption" ref="mySwiper">
+   <div class="wrapper">
+     <swiper :options="swiperOption" ref="mySwiper">
     <!-- slides -->
-    <swiper-slide>I'm Slide 1</swiper-slide>
-    <swiper-slide>I'm Slide 2</swiper-slide>
-    <swiper-slide>I'm Slide 3</swiper-slide>
-    <swiper-slide>I'm Slide 4</swiper-slide>
-    <swiper-slide>I'm Slide 5</swiper-slide>
-    <swiper-slide>I'm Slide 6</swiper-slide>
-    <swiper-slide>I'm Slide 7</swiper-slide>
+    <swiper-slide v-for="item in banerList" :key="item.id" v-if="banerList.length">
+      <img class="swiper-img" :src="item.imgUrl"/>
+    </swiper-slide>
     <!-- Optional controls -->
     <div class="swiper-pagination"  slot="pagination"></div>
   </swiper>
+   </div>
 </template>
 
 <script>
@@ -22,9 +20,28 @@
            pagination: '.swiper-pagination',
            loop: true,
            autoplay: 3000,
-           autoplayDisableOnInteraction: false
-        }
+           autoplayDisableOnInteraction: true
+        },
+        banerList:[]
       }
+    },
+    created(){
+      this.banerList.push({
+        "id": "0001",
+        "imgUrl": "http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg"
+      },{
+        "id": "0002",
+        "imgUrl": "http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg"
+      },{
+        "id": "0003",
+        "imgUrl": "http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg"
+      },{
+        "id": "0004",
+        "imgUrl": "http://img1.qunarzz.com/piao/fusion/1712/91/a275569091681d02.jpg_640x200_0519ccb9.jpg"
+      })
+    },
+    mounted(){
+      console.log("kk")
     },
     computed: {
       swiper() {
@@ -34,6 +51,15 @@
   }
 </script>
 
-<style scoped lang="stylus">
-	
+<style scoped lang="stylus" rel="stylesheet/stylus">
+.wrapper >>> .swiper-pagination-bullet-active
+  background: #fff
+.wrapper
+  overflow: hidden
+  width: 100%
+  height: 0
+  padding-bottom: 31.25%
+  background:#eee
+  .swiper-img
+    width:100% 
 </style>
